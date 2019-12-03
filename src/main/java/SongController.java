204df8ccb60df25e88f6010e5a443f010b782fcc
Elie Hess <eliemhess@gmail.com>
2019-12-03 17:26:48 -0500

    Add functional buttons (#1)
    * Add basic comment class
    
    * Add basic button methods
    
    * Update button functionality
    
    * Clear and Highlight buttons now change visible/disable
    
    * Add preliminary Delete button
    
    * Disable/Enable main menu as needed
    
    * Now store song data persistently
    
    * Update colors
    
    * Move creation of DeleteButton and Marker
    
    * Add confirmation to clear button
    
    * Add message for still-in-development features

--- src/main/java/SongController.java
+++ src/main/java/SongController.java
@@ -1,11 +1,13 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,71 +16,267 @@ import java.util.ResourceBundle;
 public class SongController implements Initializable {
 
     @FXML
-    private ImageView image;
+    private ImageView background;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button highlightButton;
     @FXML
     private Button commentButton;
     @FXML
+    private Button clearButton;
+    @FXML
     private AnchorPane anchor;
+    @FXML
+    private GridPane clearConfirmGrid;
+    @FXML
+    private GridPane featureDevGrid;
+    @FXML
+    private Label featureDevLabel;
 
-    private TextArea commentBox = null;
-    private boolean commentSelected = false;
+    private Comment comment = null;
+    private boolean commentButtonSelected = false;
+
+    private static final int COMMENT_WIDTH = 201;
+    private static final int COMMENT_HEIGHT = 120;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String songName = Main.getSongName();
-        switch (songName) {
-            case "AlmaMater":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/AlmaMater.png")));
-                break;
-            case "CrazyTrain":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/CrazyTrain.png")));
-                break;
-            case "EyeOfTheTiger":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/EyeOfTheTiger.png")));
-                break;
-            case "SevenNationArmy":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/SevenNationArmy.png")));
-                break;
-            case "SweetCaroline":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/SweetCaroline.png")));
-                break;
-            case "TakeOnMe":
-                image.setImage(new Image(getClass().getResourceAsStream("Images/TakeOnMe.png")));
-                break;
-            default:
-                System.out.println("ERROR: Invalid song name");
-        }
+        if (songName != null)
+            background.setImage(new Image(getClass().getResourceAsStream("Images/" + songName + ".png")));
     }
 
     @FXML
     private void back() {
+        Main.storeScene(background.getScene());
+
         try {
             Main.loadHome();
         } catch (IOException e) {
             System.out.println("ERROR: IOException Detected");
+            e.printStackTrace();
         }
     }
 
     @FXML
     private void commentButtonClicked() {
-        if (commentSelected)
-            commentButton.setStyle("");
+        if (comment == null)
+            toggleCommentButton();
         else
-            commentButton.setStyle("-fx-background-color: linear-gradient(#666666, #222222);-fx-text-fill: white");
-        commentSelected = !commentSelected;
+            showFeatureDev("Adding multiple comments is still in development");
+    }
+
+    @FXML
+    private void highlightButtonClicked() {
+        showFeatureDev("Highlighting is still in development");
+    }
+
+    @FXML
+    private void clearButtonClicked() {
+        backButton.setDisable(true);
+        highlightButton.setDisable(true);
+        commentButton.setDisable(true);
+        clearButton.setDisable(true);
+        clearConfirmGrid.setVisible(true);
+
+        if (comment != null)
+            comment.hideMarker();
     }
 
     @FXML
     private void imageClicked(MouseEvent mouseEvent) {
-        if (!commentSelected) return;
+        if (!commentButtonSelected) return;
 
-        if(commentBox == null) {
-            commentBox = new TextArea();
-            anchor.getChildren().add(commentBox);
-            commentBox.setVisible(true);
-            commentBox.setPrefSize(200, 150);
+        commentButtonClicked();
+        if (comment == null)
+            comment = new Comment(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+    }
+
+    @FXML
+    private void noClear() {
+        backButton.setDisable(false);
+        highlightButton.setDisable(false);
+        commentButton.setDisable(false);
+        clearConfirmGrid.setVisible(false);
+        if (comment != null)
+            comment.showMarker();
+        updateClearButton();
+    }
+
+    @FXML
+    private void yesClear() {
+        if (comment != null) comment.delete();
+
+        backButton.setDisable(false);
+        highlightButton.setDisable(false);
+        commentButton.setDisable(false);
+        clearButton.setDisable(false);
+        clearConfirmGrid.setVisible(false);
+        updateClearButton();
+    }
+
+    @FXML
+    private void featureDevOK() {
+        featureDevGrid.setVisible(false);
+        if (comment != null)
+            comment.showMarker();
+    }
+
+    private void toggleCommentButton() {
+        if (commentButtonSelected)
+            commentButton.setStyle("");
+        else
+            commentButton.setStyle("-fx-background-color: linear-gradient(#99cfff, #66a0ff);");
+
+        commentButtonSelected = !commentButtonSelected;
+    }
+
+    private void showFeatureDev(String label) {
+        featureDevLabel.setText(label);
+        featureDevGrid.setVisible(true);
+        if (comment != null)
+            comment.hideMarker();
+    }
+
+    private void updateClearButton() {
+        clearButton.setDisable(comment == null);
+    }
+
+    private class Comment {
+        double realXPos;
+        double realYPos;
+        TextArea text;
+        Button doneButton;
+        Button cancelButton;
+        Button deleteButton;
+        ImageView marker;
+        boolean thisExists = false;
+        String backupText = "";
+
+        Comment(double xPos, double yPos) {
+            this.realXPos = xPos;
+            this.realYPos = yPos;
+
+            if (xPos > Main.WIDTH - (COMMENT_WIDTH * 1.5))
+                xPos = Main.WIDTH - (COMMENT_WIDTH * 1.5);
+            if (yPos > Main.HEIGHT - (COMMENT_HEIGHT * 1.5))
+                yPos = Main.HEIGHT - (COMMENT_HEIGHT * 1.5);
+
+            text = new TextArea();
+            text.setPrefSize(COMMENT_WIDTH, COMMENT_HEIGHT);
+            text.relocate(xPos, yPos);
+            text.setWrapText(true);
+            text.setStyle("-fx-border-color: black");
+
+            doneButton = new Button();
+            doneButton.setText("Done");
+            doneButton.setPrefSize((float) COMMENT_WIDTH / 3, 25);
+            doneButton.relocate(xPos + (2 * (float) (COMMENT_WIDTH / 3)), yPos - 27);
+            doneButton.setOnAction(e -> doneClicked());
+
+            cancelButton = new Button();
+            cancelButton.setText("Cancel");
+            cancelButton.setPrefSize((float) COMMENT_WIDTH / 3, 25);
+            cancelButton.relocate(xPos, yPos - 27);
+            cancelButton.setOnAction(e -> cancelClicked());
+
+            deleteButton = new Button();
+            deleteButton.setText("Delete");
+            deleteButton.setPrefSize((float) COMMENT_WIDTH / 3, 25);
+            deleteButton.relocate(xPos + (float) COMMENT_WIDTH / 3, yPos - 27);
+            deleteButton.setOnAction(e -> delete());
+            deleteButton.setVisible(false);
+
+            marker = new ImageView();
+            marker.setImage(new Image(getClass().getResourceAsStream("Images/Comment.png")));
+            marker.relocate(realXPos - 7, realYPos - 25);
+            marker.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                markerClicked();
+                event.consume();
+            });
+            marker.setVisible(false);
+
+            anchor.getChildren().add(text);
+            anchor.getChildren().add(doneButton);
+            anchor.getChildren().add(cancelButton);
+            anchor.getChildren().add(deleteButton);
+            anchor.getChildren().add(marker);
+
+            backButton.setDisable(true);
+            highlightButton.setDisable(true);
+            commentButton.setDisable(true);
+            clearButton.setDisable(true);
         }
 
-        commentBox.relocate(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+        void hideMarker() {
+            marker.setVisible(false);
+        }
+
+        void showMarker() {
+            marker.setVisible(true);
+        }
+
+        void doneClicked() {
+            if (text.getText().trim().equals("")) {
+                cancelClicked();
+                return;
+            }
+
+            text.setVisible(false);
+            doneButton.setVisible(false);
+            cancelButton.setVisible(false);
+            deleteButton.setVisible(false);
+            marker.setVisible(true);
+
+            backupText = text.getText();
+
+            thisExists = true;
+            backButton.setDisable(false);
+            highlightButton.setDisable(false);
+            commentButton.setDisable(false);
+            updateClearButton();
+        }
+
+        void cancelClicked() {
+            if (thisExists) {
+                text.setText(backupText);
+                text.setVisible(false);
+                doneButton.setVisible(false);
+                cancelButton.setVisible(false);
+                deleteButton.setVisible(false);
+                marker.setVisible(true);
+                backButton.setDisable(false);
+                highlightButton.setDisable(false);
+                commentButton.setDisable(false);
+                updateClearButton();
+            } else delete();
+        }
+
+        void markerClicked() {
+            backButton.setDisable(true);
+            highlightButton.setDisable(true);
+            commentButton.setDisable(true);
+            clearButton.setDisable(true);
+
+            marker.setVisible(false);
+            text.setVisible(true);
+            doneButton.setVisible(true);
+            cancelButton.setVisible(true);
+            deleteButton.setVisible(true);
+        }
+
+        void delete() {
+            anchor.getChildren().remove(text);
+            anchor.getChildren().remove(marker);
+            anchor.getChildren().remove(doneButton);
+            anchor.getChildren().remove(cancelButton);
+            anchor.getChildren().remove(deleteButton);
+            comment = null;
+            backButton.setDisable(false);
+            highlightButton.setDisable(false);
+            commentButton.setDisable(false);
+            updateClearButton();
+        }
     }
 }
